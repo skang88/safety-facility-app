0deb9e581fd4bd4242d46a320f479ffb211fe59c
Seokgyun Kang <seokgyun.kang2@gmail.com>
2026-07-21 13:19:07 +0900

    환경변수로 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 80,
-    proxy: {
-      '/api': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: parseInt(env.VITE_PORT || '80'),
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://backend:5000',
+          changeOrigin: true,
+        },
+        '/uploads': {
+          target: env.VITE_API_URL || 'http://backend:5000',
+          changeOrigin: true,
+        }
       }
     }
   }
